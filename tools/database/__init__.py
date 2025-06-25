3f965f7f24f23fd2e7f00e1e75eb9a9c6a2439f7
emersonsvale <emersonsousa.vale@gmail.com>
2025-06-25 13:48:24 -0300

    Corrige import da classe DatabaseTools no __init__.py

--- tools/database/__init__.py
+++ tools/database/__init__.py
@@ -3,3 +3,4 @@ from .inserts import execute_insert
 from .updates import execute_update
 from .deletes import execute_delete
 from .tables import execute_list_tables, execute_get_project_info 
+from ..database import DatabaseTools 
